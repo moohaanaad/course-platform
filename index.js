db24fdfdd5d2321fd11b974f5fafdd1bbc0b7d0b
Mohanad Ahmed <mohanadahmed266@gmail.com>
2025-12-03 18:33:19 +0200

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-import express from 'express'
-import server from './src/server.js'
 import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import server from './src/server.js'
 const app = express()
 const port = 3000
 
 dotenv.config({ path: './config/.env', quiet: true })
+
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE,PATCH"
+}));
 
 server(app, express)
 
